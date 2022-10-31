9540404393d0d962e6d216b69d19347d0a83583d
EduardoMoraton <90452631+EduardoMoraton@users.noreply.github.com>
2022-10-31 15:01:57 +0100

    Agregar archivos de proyecto.

---
+++ BooleanToColorStringConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace T4Superheroes
+{
+    class BooleanToColorStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool state = (bool)value;
+            if (state)
+            {
+                return "LightGreen";
+            } else
+            {
+                return "Red";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace T4Superheroes
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Superheroe> list;
+        int listIndex;
+        Superheroe currentHeroe;
+        String navStr;
+        public MainWindow()
+        {
+            list = Superheroe.GetSamples();
+            InitializeComponent();
+            navStr = listIndex + 1 + "/" + list.Count();
+            listIndex = 0;
+            currentHeroe = list[listIndex];
+            mainDockPanel.DataContext = currentHeroe;
+        }
+
+        private void prevArrowImage_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            manageList(-1);
+        }
+
+        private void NextArrowImage_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            manageList(1);
+        }
+
+        private void manageList(int op)
+        {
+            int listsize = list.Count();
+            int i = listIndex;
+            i += op;
+            if (i < 0)
+                i = 0;
+            else if (i > list.Count() - 1)
+                i = list.Count() - 1;
+
+            listIndex = i;
+            currentHeroe.Nombre = list[listIndex].Nombre;
+            currentHeroe.Imagen = list[listIndex].Imagen;
+            currentHeroe.Xmen = list[listIndex].Xmen;
+            currentHeroe.Heroe = list[listIndex].Heroe;
+            currentHeroe.Vengador = list[listIndex].Vengador;
+            navStr = listIndex + 1 + "/" + list.Count();
+            currentSlideIndicatortextBlock.Text = navStr;
+
+        }
+    }
+}
